9faccc9c6a61398de3c1fcd7182e0d1567a929d8
Renard Nocturne <yanicehgpro@yahoo.com>
2021-06-13 18:50:27 +0200

    feat: :sparkles: Ajout d'animations au scroll.

--- main.js
+++ main.js
@@ -5,3 +5,73 @@ window.onscroll = function(){
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     progress.style.height = progressHeight + '%';
 }
+
+// SR
+
+ScrollReveal().reveal('.entryTitle', {
+    duration: 1000,
+    distance: '100%',
+    origin: 'top',
+    scale: 0.5,
+});
+
+ScrollReveal().reveal('.links', {
+    duration: 1000,
+    distance: '100%',
+    origin: 'right',
+    scale: 0.7,
+});
+
+ScrollReveal().reveal('.img1', {
+    duration: 1000,
+    distance: '100%',
+    origin: 'left',
+    scale: 0.7,
+    delay: 250,
+});
+
+ScrollReveal().reveal('.content1', {
+    duration: 1000,
+    distance: '75%',
+    origin: 'top',
+    scale: 0.7,
+    delay: 250,
+});
+
+ScrollReveal().reveal('.content2', {
+    duration: 1000,
+    distance: '-25%',
+    origin: 'left',
+    scale: 0.7,
+    delay: 900,
+});
+
+ScrollReveal().reveal('.img2', {
+    duration: 1000,
+    distance: '50%',
+    origin: 'right',
+    scale: 0.7,
+    delay: 900,
+});
+
+ScrollReveal().reveal('.content3', {
+    duration: 1000,
+    distance: '50%',
+    origin: 'bottom',
+    scale: 0.7,
+});
+
+ScrollReveal().reveal('.img3', {
+    duration: 1500,
+    distance: '50%',
+    origin: 'left',
+    scale: 0.7,
+});
+
+ScrollReveal().reveal('.jump', {
+    duration: 1500,
+    distance: "50%",
+    origin: 'right',
+    scale: 0.7,
+    delay: 200,
+});
